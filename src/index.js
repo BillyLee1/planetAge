c7b1d26c09531ced22d1619c9a5fd591643e5c2a
Billy Lee <codingbillylee@gmail.com>
2023-03-11 23:01:14 -0800

    Add space background. Fix sentence errors in UI

--- src/index.js
+++ src/index.js
@@ -18,28 +18,28 @@ let handlerFunction = () => {
 let displayAnswers = () => {
   let mercury = document.querySelector("#mercury");
   if (age.ageDelta > age.age) {
-    mercury.innerText = "You are " + age.mercury() + " years old on Mercury. " + age.ageDeltaFuture + " years must pass on Mercury to be " + age.ageDelta + ".";
+    mercury.innerText = "You are " + age.mercury() + " years old on Mercury. " + age.ageDeltaFuture + " years must pass on Mercury to be " + age.ageDelta + " on Earth.";
   } else if (age.ageDelta < age.age){
     mercury.innerText = "You are " + age.mercury() + " years old on Mercury. " + age.ageDeltaPast + " years on Mercury have passed since you were " + age.ageDelta;
   }
 
   let venus = document.querySelector("#venus");
   if (age.ageDelta > age.age) {
-    venus.innerText = "You are " + age.venus() + " years old on Venus. " + age.ageDeltaFuture + " years must pass on Venus to be " + age.ageDelta + ".";
+    venus.innerText = "You are " + age.venus() + " years old on Venus. " + age.ageDeltaFuture + " years must pass on Venus to be " + age.ageDelta + " on Earth.";
   } else if (age.ageDelta < age.age){
     venus.innerText = "You are " + age.venus() + " years old on Venus. " + age.ageDeltaPast + " years on Venus have passed since you were " + age.ageDelta;
   }
 
   let mars = document.querySelector("#mars");
   if (age.ageDelta > age.age) {
-    mars.innerText = "You are " + age.mars() + " years old on Mars. " + age.ageDeltaFuture + " years must pass on Mars to be " + age.ageDelta + ".";
+    mars.innerText = "You are " + age.mars() + " years old on Mars. " + age.ageDeltaFuture + " years must pass on Mars to be " + age.ageDelta + " on Earth.";
   } else if (age.ageDelta < age.age){
     mars.innerText = "You are " + age.mars() + " years old on Mars. " + age.ageDeltaPast + " years on Mars have passed since you were " + age.ageDelta;
   }
 
   let jupiter = document.querySelector("#jupiter");
   if (age.ageDelta > age.age) {
-    jupiter.innerText = "You are " + age.jupiter() + " years old on Jupiter. " + age.ageDeltaFuture + " years must pass on Jupiter to be " + age.ageDelta + ".";
+    jupiter.innerText = "You are " + age.jupiter() + " years old on Jupiter. " + age.ageDeltaFuture + " years must pass on Jupiter to be " + age.ageDelta + " on Earth.";
   } else if (age.ageDelta < age.age){
     jupiter.innerText = "You are " + age.jupiter() + " years old on Jupiter. " + age.ageDeltaPast + " years on Jupiter have passed since you were " + age.ageDelta;
   }
