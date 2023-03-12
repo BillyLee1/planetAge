c7f27fc67858be37724f296935b471a003de9020
Billy Lee <codingbillylee@gmail.com>
2023-03-12 00:48:44 -0800

    ERROR FIX: Fix random negative outputs

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import Age from "./age.js";
 
 let age = new Age();
 
-let handlerFunction = () => {
+let handlerFunction = (event) => {
   event.preventDefault();
   let orgAge = document.querySelector("#orgAge").value;
   let ageDelta = document.querySelector("#ageDelta").value;
-  age.age = orgAge;
-  age.ageDelta = ageDelta;
+  age.age = parseInt(orgAge);
+  age.ageDelta = parseInt(ageDelta);
 
   displayAnswers();
 };
@@ -23,7 +23,18 @@ let displayAnswers = () => {
   let mars = document.querySelector("#mars");
   let jupiter = document.querySelector("#jupiter");
 
-  if (age.ageDelta > age.age) {
+  if (age.ageDelta < age.age) {
+    earth.innerText = (age.age - age.ageDelta) + "  years has passed since you were " + age.ageDelta + " years old on Earth.";
+
+    mercury.innerText = "You are " + age.mercury() + " years old on Mercury. " + age.ageDeltaPast + " years on Mercury have passed since you were " + age.ageDelta;
+
+    venus.innerText = "You are " + age.venus() + " years old on Venus. " + age.ageDeltaPast + " years on Venus have passed since you were " + age.ageDelta;
+
+    mars.innerText = "You are " + age.mars() + " years old on Mars. " + age.ageDeltaPast + " years on Mars have passed since you were " + age.ageDelta;
+
+    jupiter.innerText = "You are " + age.jupiter() + " years old on Jupiter. " + age.ageDeltaPast + " years on Jupiter have passed since you were " + age.ageDelta;
+    
+  } else if (age.age < age.ageDelta) {
     earth.innerText = (age.ageDelta - age.age) + " more years on Earth until you are " + age.ageDelta + " years old.";
 
     mercury.innerText = "You are " + age.mercury() + " years old on Mercury. " + age.ageDeltaFuture + " years must pass on Mercury to be " + age.ageDelta + " on Earth.";
@@ -34,17 +45,6 @@ let displayAnswers = () => {
 
     jupiter.innerText = "You are " + age.jupiter() + " years old on Jupiter. " + age.ageDeltaFuture + " years must pass on Jupiter to be " + age.ageDelta + " on Earth.";
 
-
-  } else if (age.ageDelta < age.age){
-    earth.innerText = (age.age - age.ageDelta) + "  years has passed since you were " + age.ageDelta + " years old on Earth.";
-
-    mercury.innerText = "You are " + age.mercury() + " years old on Mercury. " + age.ageDeltaPast + " years on Mercury have passed since you were " + age.ageDelta;
-
-    venus.innerText = "You are " + age.venus() + " years old on Venus. " + age.ageDeltaPast + " years on Venus have passed since you were " + age.ageDelta;
-
-    mars.innerText = "You are " + age.mars() + " years old on Mars. " + age.ageDeltaPast + " years on Mars have passed since you were " + age.ageDelta;
-
-    jupiter.innerText = "You are " + age.jupiter() + " years old on Jupiter. " + age.ageDeltaPast + " years on Jupiter have passed since you were " + age.ageDelta;
   }
 };
 
