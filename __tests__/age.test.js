0f2e40b24bd2c68339221c526a19fbf3ceb2ce6b
Billy Lee <codingbillylee@gmail.com>
2023-03-11 17:39:06 -0800

    Add mercury age property and initial test for age property

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -3,7 +3,13 @@ import Age from "../src/age.js";
 describe('Age', () => {
 
   test('It should return an age in the object', () => {
-    let age = Age();
-    expect().toEqual(21);
+    let ages = new Age(21);
+    expect(ages.age).toEqual(21);
   });
+
+  test('It should return the age of person on Mercury'), () => {
+    let ages = new Age(21);
+    expect(ages.age).toEqual(21);
+    expect(ages.altAge).toEqual(5.04);
+  }
 });
--- src/age.js
+++ src/age.js
@@ -2,3 +2,8 @@ export default function Age(age, altAge) {
   this.age = age;
   this.altAge = altAge;
 }
+
+Age.prototype.mercury = () => {
+  this.ageAlt = .24 * this.age;
+  return ageAlt;
+}
