1d21122ed0d186d10f595f2cbe754f9b29388b61
Billy Lee <codingbillylee@gmail.com>
2023-03-11 17:17:28 -0800

    Add initial age object and test file

---
+++ __tests__/age.test.js
@@ -0,0 +1,9 @@
+import Age from "./../src/age.js";
+
+describe('Age', () => {
+
+  test('it should ', () => {
+
+    expect().toEqual();
+  });
+});
---
+++ src/age.js
@@ -0,0 +1,4 @@
+export default function Age(age, altAge) {
+  this.age = age;
+  this.altAge = altAge;
+}
